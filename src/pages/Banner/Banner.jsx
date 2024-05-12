04ac5f8792fea966b28a1c2bb4d94ba798cbd69f
mim chowdhury <umac3333@gmail.com>
2024-05-12 16:55:18 +0600

    banner-added

---
+++ src/pages/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import bg from '../../../public/banner.jpg';
+import 'animate.css';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div >
+            {/* <h1 className='' >We are best known for our public Q&A platform that millions of people visit every month to ask questions, learn, and share technical knowledge.</h1> */}
+            <div className="hero min-h-[500px]" style={{ backgroundImage: `url(${bg})` }}>
+                <div className="hero-overlay bg-opacity-40"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold animate__animated animate__fadeInDown">Informa<span className='text-blue-500'>T</span>ica</h1>
+                        <p className="mb-5 font-bold">We’re best known for our public Q&A platform that millions of people visit every month to ask questions, learn, and share information.</p>
+                        <Link to='/quries'> <button className="btn bg-blue-400 text-black">All Queries</button></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Reviews from '../Reviews/Reviews';
 import Stats from '../Stats/Stats';
+import Banner from '../Banner/Banner';
 
 const Home = () => {
     return (
         <div>
 
-
+            <Banner></Banner>
             <Reviews></Reviews>
             <Stats></Stats>
 
